bc9afeb36fc07c12d1b7f00cc75df194015c0e37
Natalia Zvereva <xx029xx@ya.ru>
2023-01-26 11:00:38 +0300

    initial commit2

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public double calculate(double m, double h) {
-        double bmi = m / (h * h);
+    public double calculate(double mass, double height) {
+        double bmi = mass / (height * height);
         return bmi;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         BmiService service = new BmiService();
-        double m = 65.0;
-        double h = 1.65;
-        double bmi = service.calculate(m, h);
+        double mass = 65.0;
+        double height = 1.65;
+        double bmi = service.calculate(mass, height);
 
         System.out.println("Индекс массы тела: " + bmi);
     }
